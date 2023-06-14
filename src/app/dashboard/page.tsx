d12164834b7e1a413eab69d4cecdeecd6926603e
Alphonso Bradham <bradhamalphonso@gmail.com>
2023-06-14 10:35:25 -0400

    Working firestore integration

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,53 @@
 "use client";
 
 import { NextPage } from "next";
+import "firebase/firestore";
+import "firebase/auth";
+import { getAuth } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  limit,
+  orderBy,
+} from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useRouter } from "next/navigation";
 import { initFirebase } from "../../../firebase/firebaseApp";
 
 const Dashboard: NextPage = () => {
+  const app = initFirebase();
+  const auth = getAuth(app);
+  const [user, loading] = useAuthState(auth);
+
+  const recipesRef = collection(getFirestore(app), "all_recipes");
+  const q = query(recipesRef, orderBy("name"), limit(3));
+
+  const [recipes] = useCollectionData(q);
+
+  const router = useRouter();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) {
+    router.push("/");
+    return <div>Please Sign in!</div>;
+  }
+
+  console.log(recipes);
   return (
     <main>
       <h1>Hello from Second Page</h1>
+      <button onClick={() => auth.signOut()}>Sign out</button>
+      <div>
+        {recipes?.map((recipe, i) => (
+          <p key={i}>{recipe["name"]}</p>
+        ))}
+      </div>
     </main>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,31 @@ import { NextPage } from "next";
 import styles from "./page.module.css";
 import { initFirebase } from "../../firebase/firebaseApp";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 const Home: NextPage = () => {
   // Instance of firebase app and auth provider
   const app = initFirebase();
   const provider = new GoogleAuthProvider();
+  const router = useRouter();
 
   // Instance of firebase Auth client
   const auth = getAuth();
+
+  // Get user state and act immediately when user's auth state changes
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return <div>Loading</div>;
+  }
+
+  // Reroute to dashboard page on signed in auth state
+  if (user) {
+    router.push("/dashboard");
+    return <div>Loading...</div>;
+  }
 
   const signIn = async () => {
     const result = await signInWithPopup(auth, provider);
